3324a186c04bac436c2d4137462b82641a20a811
tsharry2201 <141011128+tsharry2201@users.noreply.github.com>
2025-03-28 09:23:43 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/cs/research-related/AMSkt_cpp-main/AMSkt_cpp-main
-# Build directory: E:/cs/research-related/AMSkt_cpp-main/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/singleset")
-subdirs("src/multiset")
-subdirs("src/utils")
